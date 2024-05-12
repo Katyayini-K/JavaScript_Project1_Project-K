f79e3fad6c2a4100884f7721ec3dcd7bd803fae7
AmbikaKachetti <ambika9339@gmail.com>
2024-05-12 15:53:19 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+// Onclick Events
+function greenBtn(){
+    document.body.style.backgroundColor = "#5eca5e";
+}
+
+function violetBtn(){
+    document.body.style.backgroundColor = "#e09eff";
+}
+
+function yellowBtn(){
+    document.body.style.backgroundColor = "#fcfc78";
+}
+function reset(){
+    document.body.style.backgroundColor = "#ffffff";
+}
